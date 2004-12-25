3869f77549cd47fde246ca456d1df81d82fdfba6
kiyoka <kiyoka>
2004-12-25 07:55:20 +0000

    *** empty log message ***

--- lib/sumibi-define.scm
+++ lib/sumibi-define.scm
@@ -2,5 +2,5 @@
 (define sumibi-version "0.0.1")
 (define driver (dbi-make-driver "mysql"))
 
-;; 評価バランス ( 1重マルコフ 2重マルコフ skip-2重マルコフ)
-(define sumibi-evaluate-balance '(0.1 0.3 0.6))
+;; 評価バランス ( skip-2重マルコフ 2重マルコフ 1重マルコフ 2重マルコフ skip-2重マルコフ)
+(define sumibi-evaluate-balance '(0.2 0.3 0.1 0.3 0.2))
