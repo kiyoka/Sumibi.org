e85bdd1c94186b8251492585d765dfda8aa1dd1e
kiyoka <kiyoka@sumibi.org>
2004-12-26 14:17:24 +0000

    *** empty log message ***

--- lib/sumibi-define.scm
+++ lib/sumibi-define.scm
@@ -3,4 +3,7 @@
 (define driver (dbi-make-driver "mysql"))
 
 ;; 評価バランス ( skip-2重マルコフ 2重マルコフ 1重マルコフ 2重マルコフ skip-2重マルコフ)
-(define sumibi-evaluate-balance '(0.2 0.3 0.1 0.3 0.2))
+(define sumibi-evaluate-balance   '(0.2 0.3 0.1 0.3 0.2))
+
+;; 送りがな付きの単語の評価値計数
+(define sumibi-evaluate-okurigana (/ 1.0 20.0))
