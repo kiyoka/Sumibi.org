7c4a8e775fa7c28b2162ee5bc48d1619d636d684
kiyoka <kiyoka>
2005-01-08 02:32:21 +0000

    *** empty log message ***

--- lib/sumibi-define.scm
+++ lib/sumibi-define.scm
@@ -1,3 +1,7 @@
+(use file.util)
+
+
+(define sumibi-debug #f)
 
 (define sumibi-version "0.0.1")
 (define driver (dbi-make-driver "mysql"))
@@ -7,3 +11,12 @@
 
 ;; 送りがな付きの単語の評価値計数
 (define sumibi-evaluate-okurigana (/ 1.0 30.0))
+
+;; 外部との動作インターフェースモード
+(define sumibi-interface 'std)
+
+;; ユーザー設定を読み込む
+(if (file-exists? "./.sumibi")
+    (load         "./.sumibi")
+    (when (file-exists? (expand-path "~/.sumibi"))
+	  (load         "~/.sumibi")))
