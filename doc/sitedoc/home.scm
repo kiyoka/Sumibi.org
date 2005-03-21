a56dfd27dc1f0657ff3e9794fdd498f991d5153c
kiyoka <kiyoka@sumibi.org>
2005-03-21 14:59:03 +0000

    *** empty log message ***

--- doc/sitedoc/home.scm
+++ doc/sitedoc/home.scm
@@ -13,21 +13,23 @@
     (*section
      "Sumibiとは？" #f
      (ul 
-      (li "Sumibiはオープンソースのローマ字かな漢字変換システムです。")
-      (li "Internet上のドキュメントを読み込んでひとりでに賢くなる新感覚の日本語入力方式です。")
-      (li ,W:GPL "のもとで配布されています。"))
-     (*subsection
-      "特徴" #f
-      (ul 
-       (li ""))))
+      (li "Sumibiはオープンソースのローマ字漢字変換入力メソッドです。")
+      (li "Internet上のドキュメントを読み込んでひとりでに賢くなる新感覚の変換エンジンを持っています。")
+      (li (*link "sourceforge.jp" "http://sourceforge.jp/projects/sumibi/") "にて、開発しています。")
+      (li ,W:GPL "のもとで配布されています。")))
 
     (*section 
      "とりあえず使ってみる。" #f
      (ul
       (li (*link "Webブラウザから使う" "ajax/"))
-      (li (*link "Emacsに組みこむ" "sumibi_emacs.html"))))))
+      (li (*link "Emacsに組みこむ" "sumibi_el.html"))))
 
-
+    (*section
+     "特徴" #f
+     (ul 
+      (li "ローマ字入力のみをサポートします。")
+      (li "分かち書きで文節区切りを指定します。")
+      ))))
 
 ;; ページの出力
 (output "Home" (L:body))
