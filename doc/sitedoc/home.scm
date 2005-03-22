1368b923d7407a12e2be704d48fe44ba8ae2f741
kiyoka <kiyoka>
2005-03-22 16:19:21 +0000

    *** empty log message ***

--- doc/sitedoc/home.scm
+++ doc/sitedoc/home.scm
@@ -11,25 +11,33 @@
 	"email: Kiyoka Nishiyama"))
 
     (*section
-     "Sumibiとは？" #f
-     (ul 
-      (li "Sumibiはオープンソースのローマ字漢字変換入力メソッドです。")
-      (li "Internet上のドキュメントを読み込んでひとりでに賢くなる新感覚の変換エンジンを持っています。")
-      (li (*link "sourceforge.jp" "http://sourceforge.jp/projects/sumibi/") "にて、開発しています。")
-      (li ,W:GPL "のもとで配布されています。")))
+     "Sumibiとは？"
+     "What is Sumibi?"
+     (*ja
+      (ul 
+       (li "Sumibiはオープンソースのローマ字漢字変換入力メソッドです。")
+       (li "Internet上のドキュメントを読み込んでひとりでに賢くなる新感覚の変換エンジンを持っています。")
+       (li (*link "sourceforge.jp" "http://sourceforge.jp/projects/sumibi/") "にて、開発しています。")
+       (li ,W:GPL "のもとで配布されています。")))
+     (*en
+      (ul 
+       (li "Sumibi is an opensource software. ( roman to kanji input method. )")
+       (li "Sumibi has unique kanji conversion engine. Sumibi's dictionary can be built from Intenet pages.")
+       (li "It hosted on " (*link "sourceforge.jp" "http://sourceforge.jp/projects/sumibi/") " site.")
+       (li "You can use and redistribute Sumibi under the terms of the " ,W:GPL))))
 
     (*section 
-     "とりあえず使ってみる。" #f
-     (ul
-      (li (*link "Webブラウザから使う" "ajax/"))
-      (li (*link "Emacsに組みこむ" "sumibi_el.html"))))
+     "とりあえず使ってみる。"
+     "Use it now!"
+     (*ja
+      (ul
+       (li (*link "Webブラウザから使う" "ajax/"))
+       (li (*link "Emacsに組みこむ" "sumibi_el.html"))))
+     (*en
+      (ul
+       (li (*link "on your Web browser" "ajax/"))
+       (li (*link "on Emacs editor" "sumibi_el.html")))))
 
-    (*section
-     "特徴" #f
-     (ul 
-      (li "ローマ字入力のみをサポートします。")
-      (li "分かち書きで文節区切りを指定します。")
-      ))
     ,W:sf-logo
     ))
 
