7d8ef3ff939b708ff9c4366723c7c8885f77100b
kiyoka <kiyoka>
2005-07-13 14:41:08 +0000

    *** empty log message ***

--- tests/sumiyaki.sh
+++ tests/sumiyaki.sh
@@ -3,12 +3,7 @@
 HOUR=`date +'%H'`
 GO=0
 
-
-### all day execute ( debug )
-GO=1
-###
-
-for i in 23 00 01 02 03 04 05 06 07 08 09
+for i in 01 02 03 04 05 06 07 08 09
 do
   if [ $i = $HOUR ] ; then
     GO=1
