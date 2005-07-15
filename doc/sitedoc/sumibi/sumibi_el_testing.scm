aa1a609e65e903437de266280daef285b432f836
kiyoka <kiyoka@sumibi.org>
2005-07-15 13:50:19 +0000

    *** empty log message ***

--- doc/sitedoc/sumibi/sumibi_el_testing.scm
+++ doc/sitedoc/sumibi/sumibi_el_testing.scm
@@ -14,7 +14,7 @@
      (*en
       (p "No documents in English, sorry..." ))
      (*ja
-      (p "このドキュメントは、sumibi.el 0.3.2についての解説です。")))
+      (p "このドキュメントは、sumibi.el 0.3.3についての解説です。")))
 
     (*section
      "sumibi.elの特徴"
