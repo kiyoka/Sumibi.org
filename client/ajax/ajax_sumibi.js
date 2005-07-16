093d94adc1d95a33e2621f4a059d67c17eb7f79b
ktat <ktat@cpan.org>
2005-07-16 11:13:46 +0000

    improve code and for history function

--- client/ajax/ajax_sumibi.js
+++ client/ajax/ajax_sumibi.js
@@ -3,7 +3,7 @@
 // Sumibi Ajax is a client for Sumibi server.
 //
 //   Copyright (C) 2005 ktat atusi@pure.ne.jp
-//     $Date: 2005/07/10 03:50:26 $
+//     $Date: 2005/07/16 11:13:46 $
 //
 // This file is part of Sumibi
 //
@@ -25,42 +25,71 @@
 var progress  = document.getElementById('progress'); // 進行状況を表示するブロックオブジェクト
 var ime       = document.getElementById('ime');      // IMEを表示するブロックオブジェクト
 var query     = document.getElementById('q');        // query
-var defined   = document.getElementById('defined');  // 決定ボックス
-var resultbox = document.getElementById('r');
+var hist      = document.getElementById('hist');     // 
+// var resultbox = document.getElementById('r');        // 結果テキストボックス
+// var defined   = document.getElementById('defined');  // 決定テキストボックス
 var server_type = 'unstable';
-var i;
+var C = 0;
 
 var sumibi = new SumibiSOAP(progress, ime, server_type);
+
+sumibi.historyHTML(hist);
 
 function displayResult(){
     sumibi.displayResult();
 }
 
 checkKeyInput();
-checkResultBox();
 
 function checkKeyInput(){
     if(query && query.value){
-	var q = query.value;
 	var ret;
-	if(ret = sumibi.setQueryFrom(q)){
+	if(ret = sumibi.setQueryFrom(query.value)){
 	    sumibi.doConvert(ret);
 	}
     }
     setTimeout("checkKeyInput()", 1000);
 }
 
-function checkResultBox(){
-    sumibi.displayResult();
-    setTimeout("checkResultBox()", 100);
-}
-
-function sumibi_define_candidate(){
-    // defined.value += sumibi.defineCandidate();
-    query.value = sumibi.replaceQueryByResult(query.value);
-    sumibi.ime.innerHTML = '';
-}
-
 function select_server(server){
     sumibi.server(server);
 }
+
+function sumibi_define_candidate(){
+    // 決定テキストボックスがある場合
+    // defined.value += sumibi.defineCandidate(query.value);
+    var h = location.hash;
+    h = h.replace(/^#/,'') - 0;
+    query.value = sumibi.replaceQueryByResult(query.value);
+    sumibi.ime.innerHTML = '';
+    location.hash = ++C;
+    sumibi.hb.style.display = 'inline';
+}
+
+function sumibi_forward(){
+    var h = location.hash;
+    h = h.replace(/^#/,'') - 0;
+    location.hash = sumibi.forward(h);
+}
+
+function sumibi_backward(){
+    var h = location.hash;
+    h = h.replace(/^#/,'') - 0;
+    location.hash = sumibi.backward(h);
+}
+
+//**************************************************************
+// 結果テキストボックスがある場合
+//**************************************************************
+
+// checkResultBox();
+
+// function checkResultBox(){
+//    sumibi_display_result();
+//    setTimeout("checkResultBox()", 100);
+// }
+
+// function sumibi_display_result(){
+//     resultbox.value = sumibi.displayResult();
+// }
+
