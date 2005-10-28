4098a91abf21fc627f2184b0d05198422f176d39
kiyoka <kiyoka@sumibi.org>
2005-10-28 14:18:55 +0000

    *** empty log message ***

--- sample/client/ruby/SumibiWebApiSample.rb
+++ sample/client/ruby/SumibiWebApiSample.rb
@@ -1,20 +1,54 @@
 #!/usr/local/bin/ruby
-
+#
+# "SumibiWebApiSample.rb" is a sample program.
+#
+#   Copyright (C) 2005 Kiyoka Nishyama
+#     $Date: 2005/10/28 14:18:55 $
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+#
+# [ Tested on Ruby 1.8.3 ]
+#
 require 'soap/wsdlDriver'
-wsdl = 'http://www.sumibi.org/sumibi/Sumibi_stable.wsdl'
+wsdl = 'http://www.sumibi.org/sumibi/Sumibi_testing.wsdl'
 sumibi = SOAP::WSDLDriverFactory.new(wsdl).create_rpc_driver
 
-query = "sumibi yakiniku"
+if 1 > ARGV.length
+    print "usage : SumibiWebApiSample.rb string"
+    exit( 0 )
+end
+
+query = ARGV.join( ' ' )
 sumi  = "sumi_current"
 ie    = "utf-8"
 oe    = "utf-8"
 
-p sumibi.getStatus( )
+#
+# getStatus()
+#
+print "version : ", sumibi.getStatus( ).version, "\n"
 
-print sumibi.doSumibiConvertSexp( query, sumi, ie, oe )
+#
+# doSumibiConvertSexp()
+#
+print "sexp    : ", sumibi.doSumibiConvertSexp( query, sumi, ie, oe ), "\n"
 
+#
+# doSumibiConvert()
+#
 result = sumibi.doSumibiConvert( query, sumi, ie, oe )
 
+print "time    : ", result.convertTime, "\n"
+print "dump    : \n"
 result.resultElements.each {|cand|
   printf( " cand:%d no:%d spaces:%d type:%s word:%s\n",
           cand.candidate,
@@ -24,4 +58,10 @@ result.resultElements.each {|cand|
           cand.word )
 }
 
-print sumibi.doSumibiConvertHira( query, sumi, ie, oe )
+#
+# doSumibiConvertHira()
+#
+print "hiragana: ", sumibi.doSumibiConvertHira( query, sumi, ie, oe ), "\n"
+
+
+exit 0
