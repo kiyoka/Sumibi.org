a8f0f6ff9b7143b7f58e03817c18651b9ef42bb7
kiyoka <kiyoka>
2005-11-27 13:47:48 +0000

    *** empty log message ***

--- doc/sitedoc/ajax/SumibiCustomize.js
+++ doc/sitedoc/ajax/SumibiCustomize.js
@@ -4,24 +4,30 @@ function submitGform() {sumibi_define_candidate();document.getElementById('gform
 function Sumibi_candidate_html_hook(space_array,words_array) {
     var ret = '';
     var str = '';
+    var search_str = '';
     var i;
     for(i=0; i < words_array.length; i++){
 	str += space_array[i] + words_array[i];
     }
 
-    ret = '<br>'
-    + '<div style="text-align:center;">' 
-    + '<input type="button" id="search" name="search" value="『' + str + '』でGoogle検索"'
-    + ' onClick="submitGform();">'
-    + '</div>';
-
-    ret += '<br>'
-    for(i=0; i < words_array.length; i++){
-      if ( ! includeHiragana( words_array[i] )) {
-        ret += generate_amazon( words_array[i] );
-      }
+    if( google_mode() ) {
+	ret = '<br>'
+	    + '<div style="text-align:center;">' 
+	    + '<input type="button" id="search" name="search" value="『' + str + '』でGoogle検索"'
+	    + ' onClick="submitGform();">'
+	    + '</div>';
     }
 
+    if( amazon_mode() ) {
+	ret += '<br>';
+	for(i=0; i < words_array.length; i++){
+	    if ( ! includeHiragana( words_array[i] )) {
+		ret += generate_amazon( words_array[i] );
+		search_str += ' ' + words_array[i];
+	    }
+	}
+    }
+    ret += '<br>';
     return ret;
 }
 
@@ -49,4 +55,9 @@ function includeHiragana(str){
    return false;
 }
 
-
+function generate_amazon(keyword){
+   var ret = '';
+   ret += '<iframe src=\"http://rcm-jp.amazon.co.jp/e/cm?t=kiye-22&o=9&p=8&l=st1&mode=books-jp&search=' + keyword + '&=1&fc1=&lt1=&lc1=&bg1=&f=ifr\"';
+   ret += ' marginwidth=\"0\" marginheight=\"0\" width=\"120\" height=\"240\" border=\"0\" frameborder=\"0\" style=\"border:none;\" scrolling=\"no\"></iframe>';
+   return ret;
+}
