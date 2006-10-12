a9ccb116cb067b860e2b91f71d16451d6a377c23
kiyoka <kiyoka@sumibi.org>
2006-10-12 13:10:28 +0000

    *** empty log message ***

--- doc/sitedoc/ajax/SumibiCustomize.js
+++ doc/sitedoc/ajax/SumibiCustomize.js
@@ -3,7 +3,7 @@
 // Sumibi Ajax is a client for Sumibi server.
 //
 //   Copyright (C) 2006 Kiyoka Nishiyama
-//     $Date: 2006/09/25 23:56:23 $
+//     $Date: 2006/10/12 13:10:28 $
 //
 // This file is part of Sumibi
 //
@@ -42,14 +42,56 @@ function resetEvent(event)
 }
 
 
-function Sumibi_key_process_common(event)
+function judge_key(event, check_key)
 {
     var e = getEvent(event);
     var k = getKeyCode(event);
 
+    // Ctrl + 以下のキー
+    // -----------0x0X--------------
+    // a b c d e f g h i j k l m n o 
+    // 1 2 3 4 5 6 7 8 9 A B C D E F 
+    // -----------0x1X--------------
+    // p q r s t u v w x y z
+    // 0 1 2 3 4 5 6 7 8 9 A
+
+    switch( check_key ) {
+    case 'G':
+	if (( k == 0x7 ) ||
+	    ((k == 0x47  || k == 0x67 ) && e.ctrlKey)) {
+	    return true;
+	}
+	break;
+
+    case 'Z':
+	if (( k == 0x1A ) ||
+	    ((k == 0x5A || k == 0x7A ) && e.ctrlKey)) {
+	    return true;
+	}
+	break;
+
+    case 'C':
+	if (( k == 0x3 ) ||
+	    ((k == 0x43 || k == 0x63 ) && e.ctrlKey)) {
+	    return true;
+	}
+	break;
+
+    case 'J':
+	if (( k == 0xA ) ||
+	    ((k == 0x4A || k == 0x6A ) && e.ctrlKey)) {
+	    return true;
+	}
+	break;
+    }
+    return false;
+}
+
+
+function Sumibi_key_process_common(event)
+{
     // Ctrl+G
-    if (( k == 0x7 ) ||
-	((k == 0x47 || k == 0x67 ) && e.ctrlKey)) {
+    if ( judge_key( event, 'G' )) {
 	Submit_kakutei_and_google_search( );
 
 	resetEvent( event );
@@ -57,13 +99,13 @@ function Sumibi_key_process_common(event)
     }
 
     // Ctrl+Z
-    if (( k == 0x1A ) ||
-	((k == 0x5A || k == 0x7A ) && e.ctrlKey)) {
+    if ( judge_key( event, 'Z' )) {
 	sumibi_backward( );
 
 	resetEvent( event );
 	return false;
     }
+    return true;
 }
 
 function Sumibi_key_process_in_select(event,cur_no)
@@ -81,9 +123,8 @@ function Sumibi_key_process_in_select(event,cur_no)
     var return_key=13;     //Returnキー
 
     var cand = document.getElementById('sumibi_candidate' + cur_no);
-    // Ctrl+C:次のselectボックスに移動
-    if (( k == 0x3 ) ||
-	((k == 0x43 || k == 0x63 ) && e.ctrlKey)) {
+    // Ctrl+Cまたは、Ctrl+Jで次のselectボックスに移動
+    if ( judge_key( event, 'C' ) || judge_key( event, 'J' )) {
 	cand     = document.getElementById('sumibi_candidate' + (cur_no + 1));
 	if ( null == cand ) {
 	    // queryボックスにフォーカスを戻す
@@ -127,17 +168,9 @@ function Sumibi_key_process_in_text(event)
 	return true;
     }
 
-    // Ctrl + 以下のキー
-    // -----------0x0X--------------
-    // a b c d e f g h i j k l m n o 
-    // 1 2 3 4 5 6 7 8 9 A B C D E F 
-    // -----------0x1X--------------
-    // p q r s t u v w x y z
-    // 0 1 2 3 4 5 6 7 8 9 A
 
-    // Ctrl+C
-    if (( k == 0x3 ) ||
-	((k == 0x43 || k == 0x63 ) && e.ctrlKey)) {
+    // Ctrl+Cまたは、Ctrl+J
+    if ( judge_key( event, 'C' ) || judge_key( event, 'J' )) {
 	var kakutei     = document.getElementById('sumibi_candidate0'); // kakutei button
 //	resetEvent( event ); 
 	kakutei.focus();
@@ -166,7 +199,7 @@ function Sumibi_get_kakutei_button_label( )
 
 function Sumibi_get_kouho_desc_label( )
 {
-    return '(Ctrl+C)で次ボックスへ / SPACEで次候補へ<br>';
+    return '(Ctrl+CまたはCtrl+J)で次ボックスへ / SPACEで次候補へ<br>';
 }
 
 function Submit_kakutei_and_google_search() {sumibi_define_candidate();document.getElementById('gform').submit();}
