5721abfd51ec139a591edb98a9eabe2ec989a2ee
kiyoka <kiyoka@sumibi.org>
2006-11-23 01:48:55 +0000

    *** empty log message ***

---
+++ doc/sitedoc/sumibi/romaji_table.scm
@@ -0,0 +1,41 @@
+#!/usr/local/bin/gosh
+
+
+(use sumibi.romkan)
+(load "./common.scm")
+
+
+(define (L:body)
+  `(
+    (*section
+     "ローマ字-仮名対応表"
+     "romaji - kana table"
+     (*ja
+      (ul 
+       (li "最新のSumibiエンジンのローマ字-仮名対応表です。")
+       (li "最新のtestingバージョンに追従しています。")))
+     (*en
+      (ul 
+       (li "This is a romaji - kana table. Please see Japanese page.")))
+
+     (*subsection
+      "表"
+      "table"
+      (*ja
+       (table
+        (thead
+         (tr
+          (td "ローマ字")(td "仮名")))
+        (tbody
+         ,(map
+           (lambda (x)
+             `(tr (td ,(car x))
+                  (td ,(cdr x))))
+           (generate-roman->kana-table)))))
+      (*en
+       (p
+        ("sorry , Japanese only ...")))))))
+     
+
+;; ページの出力
+(output 'romaji-table (L:body))
